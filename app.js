30c82a3eafa607176b245be77beb624cf66bd853
Glenn Gillen <me@glenngillen.com>
2013-12-22 14:40:31 -0800

    Cache charts in memcache.

--- app.js
+++ app.js
@@ -4,31 +4,47 @@ var express = require('express')
     , phantom = require('phantom')
     , temp = require('temp')
     , fs = require('fs')
+    , memjs = require('memjs')
 
 var app = express();
+var mc = memjs.Client.create();
+
 app.get('/radar.png', function(req, res){
   var search = url.parse(req.url, true).search
   var params = url.parse(req.url, true).query
-  phantom.create(function(ph) {
-    ph.createPage(function(page) {
-      page.open('./index.html'+search, function(status) {
-        if (status !== 'success') {
-          console.log('FAIL to load the address');
-          res.writeHead(500, {'Content-Type': 'image/png'});
-          res.end();
-        } else {
-          page.renderBase64('PNG', function(data) {
-            ph.exit();
-            var buffer = new Buffer(data, 'base64');
-            res.writeHead(200, {
-              'Content-Type': 'image/png',
-              'Content-Length': buffer.length
-            });
-            res.end(buffer);
-          });
-        }
+  mc.get(search, function(err, data, key) {
+    if (data) {
+      var buffer = new Buffer(data, 'base64');
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': buffer.length
       });
-    })
+      res.end(buffer);
+    } else {
+      phantom.create(function(ph) {
+        ph.createPage(function(page) {
+          page.open('./index.html'+search, function(status) {
+            if (status !== 'success') {
+              console.log('FAIL to load the address');
+              res.writeHead(500, {'Content-Type': 'image/png'});
+              res.end();
+            } else {
+              page.renderBase64('PNG', function(data) {
+                ph.exit();
+                mc.set(search, data, function(err, success) {
+                  var buffer = new Buffer(data, 'base64');
+                  res.writeHead(200, {
+                    'Content-Type': 'image/png',
+                    'Content-Length': buffer.length
+                  });
+                  res.end(buffer);
+                }, (60 * 60 * 24 * 30));
+              });
+            }
+          });
+        })
+      });
+    }
   });
 
 });
